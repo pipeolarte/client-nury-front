82a6f626d26cb3a2f90b94a2de41b53016cb6993
Andres Felipe Olarte Castiblanco <andresfelipeolartecastiblanco@BOMC9082099.local>
2022-02-13 18:03:00 -0500

    avance pantalla inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,18 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'address',
+    loadChildren: () => import('./address/address.module').then( m => m.AddressPageModule)
+  },
+  {
+    path: 'map',
+    loadChildren: () => import('./map/map.module').then( m => m.MapPageModule)
+  },
+  {
+    path: 'pending-service',
+    loadChildren: () => import('./pending-service/pending-service.module').then( m => m.PendingServicePageModule)
   }
 ];
 @NgModule({
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActionSheetController} from '@ionic/angular';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,55 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  currentModal = null;
+  tittleButtonAddress = 'Cra 92 # 9 - 14';
+
+  constructor(public actionSheetCrtl: ActionSheetController,
+              private router: Router) {}
+
+  marketing() {
+    console.log('hola');
+  }
+
+  async openSheetModal() {
+    console.log('modal');
+    const dir1 = 'Cra 92 # 9 - 14';
+    const dir2= 'Cra 30 # 10 - 24';
+    const actionSheet = await this.actionSheetCrtl.create({
+      header: 'Direcciones Guardadas',
+      buttons: [
+        {
+          text: 'Ingresar nueva Dirección',
+          icon: 'add-outline',
+          handler: () => {
+            this.router.navigate(['/address']);
+            console.log('agregar dirección');
+          }
+        },
+        {
+          text: dir1,
+          icon: 'map-outline',
+          handler: () => {
+            this.tittleButtonAddress = dir1;
+            console.log('dirección 1');
+          }
+        }, {
+          text: dir2,
+          icon: 'map-outline',
+          handler: () => {
+            this.tittleButtonAddress = dir2
+            console.log('dirección 2');
+          }
+        }, {}, {}
+      ],
+      cssClass: 'custom-css',
+      animated: true,
+      backdropDismiss: true,
+      keyboardClose: false,
+      mode: 'md'
+    });
+
+    actionSheet.present();
+  }
 
 }
